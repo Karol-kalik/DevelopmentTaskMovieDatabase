375b012491b221ae46a5bbe3068a488fec786ec3
Karol Wąsik <karol.wasik00@gmail.com>
2020-09-13 09:06:27 +0200

    Create element to show information about movie or actor

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-
   $("#display").click(function (e) {
 
     $.ajax({
@@ -11,4 +10,28 @@ $(document).ready(function () {
       }
     });
   });
+
+  const showElementBox = document.querySelector('.showElementBox')
+  const closeBtn = document.querySelector('.closeBtn')
+  const tr = [...document.querySelectorAll('tr')];
+  const h1Name = document.createElement('h1');
+  tr.forEach(item => {
+    item.addEventListener('click', () => {
+      showElementBox.classList.add('show');
+      const dataId = item.dataset.id;
+      const dataTitle = item.dataset.title;
+
+      showElementBox.appendChild(h1Name);
+      h1Name.textContent = "Movie: " + dataTitle;
+
+      closeBtn.addEventListener('click', () => {
+        showElementBox.classList.remove('show');
+        h1Name.textContent = "";
+      })
+    })
+  })
+
+
+
+
 });
